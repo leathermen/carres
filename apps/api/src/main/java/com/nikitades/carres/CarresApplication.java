4aeb198106085bc31ccee504750cd785a4687f28
Nikita Pavlovskiy <nikita.pavlovskiy@fromjimmy.com>
2022-12-24 14:34:23 +0100

    The initial commit

---
+++ apps/api/src/main/java/com/nikitades/carres/CarresApplication.java
@@ -0,0 +1,13 @@
+package com.nikitades.carres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarresApplication.class, args);
+	}
+
+}
