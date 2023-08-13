1a50f4c92411f3baf399c9528248b672e8abf9bf
Nikita Pavlovskiy <nikita.pavlovskiy@fromjimmy.com>
2023-08-13 19:34:56 +0200

    Add custom metrics

---
+++ apps/api/src/main/java/com/nikitades/carres/infrastructure/spring/scheduling/SchedulingConfig.java
@@ -0,0 +1,10 @@
+package com.nikitades.carres.infrastructure.spring.scheduling;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class SchedulingConfig {
+  
+}
