fc43e7acb325391d5349cb774af39beb6f366779
Nikita Pavlovskiy <nikita.pavlovskiy@fromjimmy.com>
2023-08-16 01:05:07 +0200

    Fix backend metrics counter

--- apps/api/src/main/java/com/nikitades/carres/infrastructure/prometheus/AnalyticsReporter.java
+++ apps/api/src/main/java/com/nikitades/carres/infrastructure/prometheus/AnalyticsReporter.java
@@ -2,19 +2,25 @@ package com.nikitades.carres.infrastructure.prometheus;
 
 import io.micrometer.core.instrument.Counter;
 import io.micrometer.core.instrument.MeterRegistry;
-import io.micrometer.core.instrument.Metrics;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 @Component
+@Slf4j
 public class AnalyticsReporter {
 
-  private final String RESERVATIONS_CREATED = "carres_api_reservations_created";
+  private static final String RESERVATIONS_CREATED = "carres_api_reservations_created";
+
+  private final MeterRegistry registry;
 
   public AnalyticsReporter(MeterRegistry registry) {
+    this.registry = registry;
     Counter.builder(RESERVATIONS_CREATED).tags("model", "", "manufacturer", "").register(registry);
   }
 
   public void addReservationCreated(String model, String manufacturer) {
-    Metrics.counter(RESERVATIONS_CREATED, "model", model, "manufacturer", manufacturer).increment();
+    registry
+      .counter(RESERVATIONS_CREATED, "model", model, "manufacturer", manufacturer)
+      .increment();
   }
 }
