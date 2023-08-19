6184a515f5f07a244a0bb6c2421df5c5e354d875
Nikita Pavlovskiy <nikita.pavlovskiy@fromjimmy.com>
2023-08-19 13:53:55 +0200

    Move to a multi-project structure

---
+++ apps/backend/tg-notifier/src/main/java/com/nikitades/carres/tgnotify/TgnotifyApplication.java
@@ -0,0 +1,28 @@
+package com.nikitades.carres.tgnotify;
+
+import com.nikitades.carres.shared.NewReservationNotification;
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+@SpringBootApplication
+@EnableRabbit
+public class TgnotifyApplication {
+
+  @Autowired
+  private WebhookCarresNotificationBot bot;
+
+  public static void main(String[] args) {
+    SpringApplication.run(TgnotifyApplication.class);
+  }
+
+  @RabbitListener(queues = "new_reservations")
+  public void receive(NewReservationNotification msg) throws TelegramApiException {
+    bot.notify(
+      "New reservation:%n%s%n%s%n%s".formatted(msg.email(), msg.manufacturer(), msg.model())
+    );
+  }
+}
